015b7291914d2e9b8b83e30d0e33dcf96114035b
yamaguchi1231 <ggf-600-vts@e-net.nara.jp>
2025-09-19 06:19:41 +0000

    javascriptの追加 ・自機の描画・移動 ・敵の移動

---
+++ game/main.js
@@ -0,0 +1,40 @@
+document.getElementById("a").innerText = "これはゲームです";
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let x = 255;
+let tama = 0;
+window.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        x -= 10;
+    } else if(e.key === "ArrowRight"){
+        x += 10;
+    }else if(e.key === "Space") {
+    tama += 1;
+}
+});
+
+let y1 = 0;
+let y2 = -150;
+
+function gameLoop(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0,canvas.width, canvas.height);
+    ctx.fillStyle = "red";
+    ctx.fillRect(150, y1, 30, 30);
+    y1 += 5;
+    ctx.fillStyle = "red";
+    ctx.fillRect(300, y2, 30, 30);
+    y2 += 5;
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(x, 480, 30, 30);
+    if (tama > 0) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(x + 10, 480 = tama * 10,10,10);
+    }
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+
